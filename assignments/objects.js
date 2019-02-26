3cb679e0ae83637ca37cd30abad0943446a2a525
Your Name <you@example.com>
2019-02-25 19:07:08 -0500

    finsihed second stretch goal

--- assignments/objects.js
+++ assignments/objects.js
@@ -99,15 +99,21 @@ console.log(antonietta.multiplyNums(3,4));
 const parent = {
   name: 'Susan',
   age: 70,
-  speak: 'Hello my name is',
+  speak: function() {
+    console.log(`My name is: ${this.name}!`)
+  },
     child: {
     name: 'George',
     age: 50,
-    speak: "Hello my name is",
-      "grandchild": {
+    speak: function() {
+      console.log(`My name is: ${this.name}!`)
+    },
+      grandchild: {
       name: 'Sam',
       age: 30,
-      speak: "Hello my name is"
+      speak: function() {
+        console.log(`My name is: ${this.name}!`)
+      },
     }
   }
 }
@@ -119,7 +125,8 @@ console.log (parent.child.name);
 // Log the name and age of the grandchild
 console.log (parent.child.grandchild);
 // Have the parent speak
-console.log (parent.speak);
+console.log (parent.speak());
 // Have the child speak
-
+console.log (parent.child.speak());
 // Have the grandchild speak
+console.log (parent.child.grandchild.speak());
